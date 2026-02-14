f9253e59a77ca32776349fd8f924705656e05e03
GAMEA9G <gamea9g@gmail.com>
2026-02-14 11:03:24 +0530

    update

---
+++ prisma/clean_dump.sql
@@ -0,0 +1,74 @@
+CREATE TABLE IF NOT EXISTS "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "Syllabus" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+INSERT INTO Syllabus VALUES(1,'dgfgf');
+INSERT INTO Syllabus VALUES(2,'fgh');
+CREATE TABLE IF NOT EXISTS "Subjects" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "SyllabusId" INTEGER NOT NULL,
+    CONSTRAINT "Subjects_SyllabusId_fkey" FOREIGN KEY ("SyllabusId") REFERENCES "Syllabus" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO Subjects VALUES(1,'vhghfh',1);
+CREATE TABLE IF NOT EXISTS "Chapter" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "SubjectId" INTEGER NOT NULL,
+    CONSTRAINT "Chapter_SubjectId_fkey" FOREIGN KEY ("SubjectId") REFERENCES "Subjects" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "Quiz" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "ChapterId" INTEGER NOT NULL,
+    CONSTRAINT "Quiz_ChapterId_fkey" FOREIGN KEY ("ChapterId") REFERENCES "Chapter" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "QandA_quiz" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "Question" TEXT NOT NULL,
+    "Option1" TEXT NOT NULL,
+    "Option2" TEXT NOT NULL,
+    "Option3" TEXT NOT NULL,
+    "Option4" TEXT NOT NULL,
+    "Answer" TEXT NOT NULL,
+    "QuizId" INTEGER NOT NULL,
+    CONSTRAINT "QandA_quiz_QuizId_fkey" FOREIGN KEY ("QuizId") REFERENCES "Quiz" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "Topic" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "ChapterId" INTEGER NOT NULL,
+    CONSTRAINT "Topic_ChapterId_fkey" FOREIGN KEY ("ChapterId") REFERENCES "Chapter" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "QandA" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "Question" TEXT NOT NULL,
+    "Option1" TEXT NOT NULL,
+    "Option2" TEXT NOT NULL,
+    "Option3" TEXT NOT NULL,
+    "Option4" TEXT NOT NULL,
+    "Answer" TEXT NOT NULL,
+    "TopicId" INTEGER NOT NULL,
+    CONSTRAINT "QandA_TopicId_fkey" FOREIGN KEY ("TopicId") REFERENCES "Topic" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "Notes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "Author" TEXT NOT NULL,
+    "Date" DATETIME NOT NULL,
+    "ChapterId" INTEGER NOT NULL,
+    CONSTRAINT "Notes_ChapterId_fkey" FOREIGN KEY ("ChapterId") REFERENCES "Chapter" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS sqlite_sequence(name,seq);
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('Syllabus',2);
+INSERT INTO sqlite_sequence VALUES('Subjects',1);
+INSERT INTO sqlite_sequence VALUES('Notes',0);
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "Syllabus_name_key" ON "Syllabus"("name");
